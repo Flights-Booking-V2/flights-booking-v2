23edcde557ba1e98f51f1520e529afea7dd60576
Mohammed AbuShaban <mohammad.d.h.2000@gmail.com>
2020-07-20 18:23:51 +0300

    just making the branch without my changes till now

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,46 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage.js";
 import Signup from "./components/signup/signup.js";
 import Signin from "./components/signin/signin.js";
 //main app 
 //used React routers here to route to the main pages
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/Signin" component={Signin} />
-          <Route path="/Signup" component={Signup} />
-        </Switch>
-      </BrowserRouter>
-    </div>
+  return ( <
+    div className = "App" >
+    <
+    BrowserRouter >
+    <
+    Switch >
+    <
+    Route exact path = "/"
+    component = {
+      HomePage
+    }
+    /> <
+    Route path = "/Signin"
+    component = {
+      Signin
+    }
+    /> <
+    Route path = "/Signup"
+    component = {
+      Signup
+    }
+    /> <
+    Route path = "/ContactUs"
+    component = {
+      ContactUs
+    }
+    /> <
+    /Switch> <
+    /BrowserRouter> <
+    /div>
   );
 }
 
