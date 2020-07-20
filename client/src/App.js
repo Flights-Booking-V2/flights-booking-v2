8a1c0fdd5864632c602cfcde09462c482c7bb483
Mohammed AbuShaban <mohammad.d.h.2000@gmail.com>
2020-07-20 19:52:03 +0300

    finishing 60% of the contact us component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Button } from "reactstrap";
 import HomePage from "./components/HomePage/HomePage.js";
 import Signup from "./components/signup/signup.js";
 import Signin from "./components/signin/signin.js";
+import ContactUs from "./components/ContactUs/ContactUs.js";
 //main app
 //used React routers here to route to the main pages
 function App() {
@@ -14,6 +16,7 @@ function App() {
           <Route exact path="/" component={HomePage} />
           <Route path="/Signin" component={Signin} />
           <Route path="/Signup" component={Signup} />
+          <Route path="/ContactUs" component={ContactUs} />
         </Switch>
       </BrowserRouter>
     </div>
